7ff1c23ce0ca4782c929dc9f27822f48dd155bc8
Maithreyi Prabhu <maithreyi.prabhu95@gmail.com>
2020-02-08 21:14:54 -0500

    Update IllumioChallenge.py

--- IllumioChallenge.py
+++ IllumioChallenge.py
@@ -1,19 +1,19 @@
 """
 Algorithm:
-    1. Create two dictionaries. One for rules containing IP [rulesWithoutIPRange] range and one for rules which does not contain IP range [rulesWithIPRange].
+    1. Create two dictionaries. One for rules containing IP [__rulesWithoutIPRange] range and one for rules which does not contain IP range [__rulesWithIPRange].
     2. When incoming packets are parsed, first check if given direction + protocol + IP is present as key in the
-    rulesWithoutIPRange. If present, check if the incoming packet port is equal to port of the given rule present as value.
+    __rulesWithoutIPRange. If present, check if the incoming packet port is equal to port of the given rule present as value.
     If true then return to main function else go to step 3.
         Time Complexity : O(1)
     3.  If direction + protocol + IP is present but port for the rule is present as a range then check if the incoming packet port
     is in the range of given ports for the rule. If true then return to main function else go to step 4.
         Time Complexity: O(1)
-    4. If direction + protocol + port is present as key in rulesWithIPRange dictionary then loop through all the IP ranges present
+    4. If direction + protocol + port is present as key in __rulesWithIPRange dictionary then loop through all the IP ranges present
     as the value. For each range of IP address, check if the incoming packet IP address is valid in the given range. If true then return
     else go to step 5.
         Time Complexity: O(N) where N: Number of rules containing only IP addresses as
         Ranges with direction + protocol + port as key
-    5. If direction + protocol  is present as key in rulesWithIPRange dictionary then loop through all the IP ranges present
+    5. If direction + protocol  is present as key in __rulesWithIPRange dictionary then loop through all the IP ranges present
     as the value.  For each range of IP address, check if the incoming packet IP address is valid in the given range.
     If true, check if the given port for the incoming packet is in range of given ports. If truem then return
     else return False
@@ -31,8 +31,8 @@ import csv
 
 class fireWallClass:
     # Intializing a dictionary called rules to store the initial given set of rules
-    rulesWithoutIPRange = {}
-    rulesWithIPRange = {}
+    __rulesWithoutIPRange = {}
+    __rulesWithIPRange = {}
     #Constructor for taking csv file name as input
     def __init__(self, rulesFileName):
         """
@@ -45,23 +45,23 @@ class fireWallClass:
             rulesFileReader = csv.reader(rulesFile, delimiter=',')
             for row in rulesFileReader:
                 if "-" in row[3] and "-" not in row[2]:
-                    if row[0]+"_"+row[1]+"_"+row[2] in self.rulesWithIPRange:
-                        self.rulesWithIPRange[row[0]+"_"+row[1]+"_"+row[2]]+=([(row[3].split('-'))])
+                    if row[0]+"_"+row[1]+"_"+row[2] in self.__rulesWithIPRange:
+                        self.__rulesWithIPRange[row[0]+"_"+row[1]+"_"+row[2]]+=([(row[3].split('-'))])
                     else:
-                        self.rulesWithIPRange[row[0] + "_" + row[1]+"_"+row[2]] = [(row[3].split('-'))]
+                        self.__rulesWithIPRange[row[0] + "_" + row[1]+"_"+row[2]] = [(row[3].split('-'))]
                 elif "-" in row[3] and "-" in row[2]:
-                    if row[0] + "_" + row[1]in  self.rulesWithIPRange:
-                        self.rulesWithIPRange[row[0] + "_" + row[1]]+=([(row[3].split('-'), row[2].split('-'))])
+                    if row[0] + "_" + row[1]in  self.__rulesWithIPRange:
+                        self.__rulesWithIPRange[row[0] + "_" + row[1]]+=([(row[3].split('-'), row[2].split('-'))])
                     else:
-                        self.rulesWithIPRange[row[0] + "_" + row[1]] = [(row[3].split('-'), row[2].split('-'))]
+                        self.__rulesWithIPRange[row[0] + "_" + row[1]] = [(row[3].split('-'), row[2].split('-'))]
                 elif "-" in row[2]:
                     y = row[2]
-                    self.rulesWithoutIPRange[row[0] + "_" + row[1] + "_" + row[3]] = y.split('-')
+                    self.__rulesWithoutIPRange[row[0] + "_" + row[1] + "_" + row[3]] = y.split('-')
                 else:
-                    self.rulesWithoutIPRange[row[0] + "_" + row[1] + "_" + row[3]] = [row[2]]
+                    self.__rulesWithoutIPRange[row[0] + "_" + row[1] + "_" + row[3]] = [row[2]]
 
 
-    def splitIP(self,address):
+    def __splitIP(self,address):
         """
         splitIP method takes the address in string format and return integers in tuple format
         :param address: String
@@ -69,7 +69,7 @@ class fireWallClass:
         """
         return tuple(int(n) for n in address.split('.'))
 
-    def checkIfIPInRange(self,addr, startRange, startEnd):
+    def __checkIfIPInRange(self,addr, startRange, startEnd):
         """
         Checks if the given IP Address is in the range.
         :param addr: String address which needs to be checked if it is in the range
@@ -77,10 +77,10 @@ class fireWallClass:
         :param startEnd: String end address in the range
         :return: Boolean value
         """
-        return self.splitIP(startRange) <= self.splitIP(addr) <= self.splitIP(startEnd)
+        return self.__splitIP(startRange) <= self.__splitIP(addr) <= self.__splitIP(startEnd)
 
 
-    def checkInWithoutIPRange(self, x, port):
+    def __checkInWithoutIPRange(self, x, port):
         """
         If IP range does not exist and IP is directly present in the dictionary as a key and port is present as a range
         in the value of the dictionary,
@@ -89,8 +89,8 @@ class fireWallClass:
         :param port: String value which contains port number to be checked for
         :return: Boolean value
         """
-        if x in self.rulesWithoutIPRange.keys():
-            value = self.rulesWithoutIPRange[x]
+        if x in self.__rulesWithoutIPRange.keys():
+            value = self.__rulesWithoutIPRange[x]
             if len(value) == 2:
                 if int(port) >= int(value[0]) and int(port)<=int(value[1]):
                     return True
@@ -99,7 +99,7 @@ class fireWallClass:
             else:
                 return False
 
-    def checkInWithIPRange(self, x, port,ip_address):
+    def __checkInWithIPRange(self, x, port,ip_address):
         """
         If IP is not directly present in the dictionary as a key and port may or may not present as a range
         in the value of the dictionary,
@@ -109,14 +109,14 @@ class fireWallClass:
         :param ip_address: String value which contains IP_Address to be checked for
         :return: Boolean value
         """
-        if x + "_" +str(port) in self.rulesWithIPRange.keys():
-            ipRanges  = self.rulesWithIPRange[x+"_"+str(port)]
-            if self.checkIfIPInRange(ip_address, ipRanges[0][0],ipRanges[0][1]):
+        if x + "_" +str(port) in self.__rulesWithIPRange.keys():
+            ipRanges  = self.__rulesWithIPRange[x+"_"+str(port)]
+            if self.__checkIfIPInRange(ip_address, ipRanges[0][0],ipRanges[0][1]):
                 return True
-        if x in self.rulesWithIPRange.keys():
-            ipRanges = self.rulesWithIPRange[x]
+        if x in self.__rulesWithIPRange.keys():
+            ipRanges = self.__rulesWithIPRange[x]
             for i in ipRanges:
-                if self.checkIfIPInRange(ip_address, i[0][0], i[0][1]) and int(port) <= int(i[1][1]) and int(port)>= int(i[1][0]):
+                if self.__checkIfIPInRange(ip_address, i[0][0], i[0][1]) and int(port) <= int(i[1][1]) and int(port)>= int(i[1][0]):
                     return True
         return False
 
@@ -130,9 +130,9 @@ class fireWallClass:
         :return: Boolean
         """
         x = direction + "_" + protocol + "_" + ip_address
-        if self.checkInWithoutIPRange(x,port):
+        if self.__checkInWithoutIPRange(x,port):
             return True
-        elif self.checkInWithIPRange(direction+"_"+protocol,port,ip_address):
+        elif self.__checkInWithIPRange(direction+"_"+protocol,port,ip_address):
             return True
         else:
             return False
